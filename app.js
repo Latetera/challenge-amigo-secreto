1edcc8e5e68e73c36ea5fe1e788b6284bc245e10
Omar <l.o.osuna@outlook.com>
2025-01-29 12:52:07 -0600

    Implementa la función para agregar amigos

--- app.js
+++ app.js
@@ -2,3 +2,19 @@
 
 let amigos = [];
 
+// función para agregar amigos
+function agregarAmigo() {
+    // mediante el DOM obtiene el texto ingresado por el usuario
+    let nombre = document.getElementById('amigo').value;
+    // valida la entrada de datos
+    // si no se ingresa un nombre no es válida
+    if (nombre == '') {
+        alert('Por favor, ingrese un nombre');
+    // en caso contrario, si se ingresa un nombre, es válida
+    } else {
+        amigos.push(nombre);
+        nombre = document.getElementById('amigo').value = '';
+    }
+    console.log(amigos);
+    actualizarLista();
+}
