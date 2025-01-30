393d707c7045c93e6975ec5235f9215c45b7423b
Omar <l.o.osuna@outlook.com>
2025-01-29 18:08:06 -0600

    Implementa la función para sortear los amigos

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ let amigos = [];
 function agregarAmigo() {
     // mediante el DOM obtiene el texto ingresado por el usuario
     let nombre = document.getElementById('amigo').value;
+
     // valida la entrada de datos
     // si no se ingresa un nombre no es válida
     if (nombre == '') {
         alert('Por favor, ingrese un nombre');
+
     // en caso contrario, si se ingresa un nombre, es válida
     } else {
         amigos.push(nombre);
@@ -32,4 +34,37 @@ function actualizarLista() {
         lista.appendChild(elementoListaAmigos);
     }
     console.log(lista);
+}
+
+// función para sortear a los amigos de la lista
+function sortearAmigo() {
+
+    // valida que la lista 'amigos' no esté vacía
+    // si no está vacía
+    if (amigos.length > 0) {
+        // Genera un índice aleatorio con un límite máximo según la cantidad de amigos ingresados
+        let indiceDelGanador = Math.floor(Math.random()*amigos.length-1)+1;
+        console.log(indiceDelGanador);
+        // Obtiene el nombre del ganador
+        let nombreDelGanador = amigos[indiceDelGanador];
+        console.log(nombreDelGanador);
+        // muestra el nombre del ganador
+        // Mediante el DOM selecciona el elemento correspondiente
+        let mensajeDelGanador = document.getElementById('resultado');
+        // Mediante el DOM y template string muestra el resultado en el elemento seleccionado
+        mensajeDelGanador.innerHTML = `El amigo secreto sorteado es: ${nombreDelGanador}`;
+
+        // Limpia el elemento que contiene los elementos 'li'
+        console.log(amigos.length);
+        for (let j = 0; j < amigos.length; j++) {
+            document.querySelector('li').remove();
+        }
+        // Re inicializa la lista 'amigos'
+        amigos = [];
+        console.log(amigos.length);
+
+    // En caso contrario, si la lista 'amigos' está vacía
+    } else {
+        alert('La lista está vacía, por favor inserte un nombre');
+    }
 }
